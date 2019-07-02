a83e28a896f19d6080816a7f17555202b3d52b4f
Никитос <nikitagram@gmail.com>
2019-07-02 22:49:04 +0300

    Задание три

--- js/setup.js
+++ js/setup.js
@@ -8,6 +8,9 @@ var secondNames = ['да Марья','Верон','Мирабелла','Валь
 var coatColors = ['rgb(101, 137, 164)','rgb(241, 43, 107)','rgb(146, 100, 161)','rgb(56, 159, 117)','rgb(215, 210, 55)','rgb(0, 0, 0)'];
 var eyesColors = ['black','red','blue','yellow','green']
 
+var similarListElement = setup.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
 var getName = function(firstWord, secondWord) {
   return firstWord[Math.ceil(Math.random()*(firstWord.length-1))] + ' ' + secondWord[Math.ceil(Math.random()*(secondWord.length-1))];
 }
@@ -37,6 +40,23 @@ var wizards = [
     coatColor: getColors(coatColors),
     eyesColor: getColors(eyesColors)
   }
-]
+];
 
-console.log(wizards);
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+}
+
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+
+similarListElement.appendChild(fragment);
+setup.querySelector('.setup-similar').classList.remove('hidden');
